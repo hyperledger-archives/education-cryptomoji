db9b59aa7e46ea005cda3773020e03918363994f
Taylor Page <jonathantaylorpage@gmail.com>
2018-03-10 10:23:26 -0600

    Refactor code to utilize es6
    Convert previous javascript written in es5 to utilize new es6+ standards
    
    Signed-off-by: Taylor Page <jonathantaylorpage@gmail.com>

--- intro/blockchain-intro-prompt.js
+++ intro/blockchain-intro-prompt.js
@@ -1,104 +1,95 @@
-var SHA256 = require('crypto-js/sha256');
+let SHA256 = require('crypto-js/sha256');
 
-// Block class
-function Block() {
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
     this._nonce = 0;
     /*
-        Initialize the block constructor
+      Initialize the block constructor
 
-        **Note**
-        The block should be initiated with
-            - an index
-            - a timestamp
-            - the stored data
-            - the previous block's hash
+      **Note**
+      The block should be initiated with
+        - an index
+        - a timestamp
+        - the stored data
+        - the previous block's hash
     */
+  }
 
-   // Calculate a hash based on data supplied to block
-   calculateHash = function () {
-       /*
-           Add functionality to return a hash by passing a compiled string into the SHA256 hashing function
-           The string that is passed in should contain specific data about the block that would be impossible to replicate
+  // Calculate a hash based on data supplied to block
+  calculateHash() {
+    /*
+      Add functionality to return a hash by passing a compiled string into the SHA256 hashing function
+      The string that is passed in should contain specific data about the block that would be impossible to replicate
 
-           **Hint**
-           Unreplicable data can include
-               - the block index
-               - the previous hash
-               - the block timestamp
-       */
-       return SHA256('hash');
-   };
+      **Hint**
+      Unreplicable data can include
+        - the block index
+        - the previous hash
+        - the block timestamp
+    */
+   return SHA256('hash');
+  }
 
-   // Mine block by difficulty
-   mineBlock = function (difficulty) {
-       var mined = false;
-       while (false) {
-           /*
-               In order to mine using proof of work the calculateHash function needs to incorporate the nonce
-               The nonce should increase everytime the function loops until the number of zeros at the beginning of the hash
-               matches the number that is passed in (difficulty)
-               The stored hash should be updated everytime the nonce increases
-           */
-           mined = true;
-       }
-       mined ? console.log('Block mined' + ' ' + this.hash) : console.log('Error');
-   };
-
-   // Inititalize using methods
-   (function init() {
-        this.hash = this.calculateHash();
-    }.bind(this))();
+  // Mine block by difficulty
+  mineBlock(difficulty) {
+    let mined = false;
+    while (!mined) {
+      /*
+          In order to mine using proof of work the calculateHash function needs to incorporate the nonce
+          The nonce should increase everytime the function loops until the number of zeros at the beginning of the hash
+          matches the number that is passed in (difficulty)
+          The stored hash should be updated everytime the nonce increases
+      */
+      mined = true;
+    }
+    mined ? console.log(`Block mined ${this.hash}`) : console.log('Error');
+  }
 }
 
-// Blockchain class
-function Blockchain() {
-    this.chain = [];
-    this._difficulty = 2;
+class Blockchain {
+  constructor() {
+    this.chain = [this._createGenesisBlock()];
+    this._difficulty = 3;
+  }
 
-    // Create the initial block for the blockchain
-    _createGenesisBlock = function () {
-        /* Add functionality to create an initial block */
-    };
+  // Create the initial block for the blockchain
+  _createGenesisBlock() {
+    /* Add functionality to create an initial block */
+  }
 
-    // Get the last created block
-    getLatestBlock = function () {
-        /* Add functionality to get the latest block in the chain */
-    };
+  // Get the last created block
+  getLatestBlock() {
+    /* Add functionality to get the latest block in the chain */
+  }
 
-    // Add a new block to the blockchain
-    addBlock = function (newBlock) {
-        /*
-            Create function that:
-                - Adds previousHash property to new block
-                - Create hash (or mine block)
-                - Adds block to the chain
-        */
-    };
+  // Add a new block to the blockchain
+  addBlock(newBlock) {
+    /*
+      Create function that:
+        - Adds previousHash property to new block
+        - Create hash (or mine block)
+        - Adds block to the chain
+    */
+  }
 
-    // Check if the chain is valid
-    validChain = function () {
-        /*
-            Add functionality that determines that:
-                - Each block's hash in the chain is valid
-                - Each block's previous hash property matches the previous block's hash
-        */
-    };
-
-    // Inititalize using methods
-    (function init() {
-        this.chain = [this._createGenesisBlock()];
-    }.bind(this))();
+  // Check if the chain is valid
+  isValidChain() {
+     /*
+      Add functionality that determines that:
+        - Each block's hash in the chain is valid
+        - Each block's previous hash property matches the previous block's hash
+    */
+  }
 }
 
 /*
+  Create a blockchain with at least 3 blocks not including the genesis block
+    Blockchain:
+    - The blockchain should return True when running validChain()
+    - getLatestBlock() should return the last block in the chain
 
-    Create a blockchain with at least 3 blocks not including the genesis block
-        Blockchain:
-        - The blockchain should return True when running validChain()
-        - getLatestBlock() should return the last block in the chain
+    Block:
+    - Each block should have specific stored data
+    - Mining a block should print the hash number (successful)
+*/
 
-        Block:
-        - Each block should have specific stored data
-        - Mining a block should print the hash number (successful)
-        
-*/ 
--- intro/blockchain-intro-solution.js
+++ intro/blockchain-intro-solution.js
@@ -1,74 +1,69 @@
-const SHA256 = require('crypto-js/sha256');
+let SHA256 = require('crypto-js/sha256');
 
-// Block class
-function Block(index, timestamp, data, previousHash) {
+class Block {
+  constructor(index, timestamp, data, previousHash = '') {
     this.index = index;
     this.timestamp = timestamp;
     this.data = data;
     this.previousHash = previousHash;
+    this.hash = this.calculateHash();
     this._nonce = 0;
-    this.hash = '';
-    
-    // Calculate a hash based on data supplied to block
-    this.calculateHash = function () {
-        return SHA256("" + this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this._nonce).toString();
-    };
-    
-    // Mine block by difficulty
-    this.mineBlock = function (difficulty) {
-        console.log('Mining block...')
-        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
-            this._nonce++;
-            this.hash = this.calculateHash();
-        }
-        console.log('Block mined' + ' ' + this.hash);
-    };
+  }
 
-    // Inititalize using methods
-    (function init() {
-        this.hash = this.calculateHash();
-    }.bind(this))();
+  // Calculate a hash based on data supplied to block
+  calculateHash() {
+    return SHA256(`${this.index}${this.previousHash}${this.timestamp}${JSON.stringify(this.data)}${this._nonce}`).toString();
+  }
+
+  // Mine block by difficulty
+  mineBlock(difficulty) {
+    console.log('Mining block...');
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join('0')) {
+      this._nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log(`${'Block mined'} ${this.hash}`);
+  }
 }
 
-// Blockchain class
-function Blockchain() {
-    this._difficulty = 5;
-    this.chain = [];
-    
-    // Create the initial block for the blockchain
-    this._createGenesisBlock = function () {
-        return new Block(0, (new Date()).toString(), 'Genesis Block', '0');
-    };
-    
-    // Get the last created block
-    this.getLatestBlock = function () {
-        return this.chain[this.chain.length - 1];
-    };
+class Blockchain {
+  constructor() {
+    this.chain = [this._createGenesisBlock()];
+    this._difficulty = 3;
+  }
 
-    // Add a new block to the blockchain
-    this.addBlock = function (newBlock) {
-        newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.mineBlock(this._difficulty);
-        this.chain.push(newBlock);
-    };
+  // Create the initial block for the blockchain
+  _createGenesisBlock() {
+    return new Block(0, (new Date()).toString(), 'Genesis Block', '0');
+  }
 
-    // Check if the chain is valid
-    this.validChain = function () {
-        var _this = this;
-        var validity = true;
+  // Get the last created block
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
 
-        this.chain.forEach(function (block, i) {
-            var previousBlock = _this.chain[i - 1];
-            if (block.hash !== block.calculateHash() ||
-                block.previousHash !== previousBlock.hash) {
-                validity = false;
-            }
-        });
-        return validity;
-    };
+  // Add a new block to the blockchain
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this._difficulty);
+    this.chain.push(newBlock);
+  }
 
-    // Inititalize using methods
-    (function init() {
-        this.chain = [this._createGenesisBlock()];
-    }.bind(this))();
+  // Check if the chain is valid
+  isValidChain() {
+    let validity = true;
+
+    this.chain.forEach((block, i) => {
+      const previousBlock = this.chain[i - 1];
+
+      if (
+        block.hash !== block.calculateHash() ||
+        block.previousHash !== previousBlock.hash
+      ) {
+        validity = false;
+      }
+    });
+
+    return validity;
+  }
 }
