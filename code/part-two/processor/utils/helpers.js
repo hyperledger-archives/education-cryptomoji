d53d6a5c69280fcd59fd0b5c7a659ba2215f8f32
Zac Delventhal <zac@bitwise.io>
2018-03-19 00:35:13 -0500

    Add create collection action to transaction handler
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-two/processor/utils/helpers.js
+++ code/part-two/processor/utils/helpers.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { createHash } = require('crypto');
+const { InvalidTransaction } = require('sawtooth-sdk/processor/exceptions');
 
 
 // Returns the first characters of a SHA-512 hash of a string
@@ -18,8 +19,16 @@ const encode = obj => {
 // Decodes JSON Buffers back into objects
 const decode = encoded => JSON.parse(encoded.toString());
 
+// Returns a rejected promise with an InvalidTransaction
+const reject = message => {
+  return new Promise((_, reject) => {
+    reject(new InvalidTransaction(message));
+  });
+};
+
 module.exports = {
   hash,
   encode,
-  decode
+  decode,
+  reject
 };
