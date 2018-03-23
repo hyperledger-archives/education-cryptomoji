c107f735b50397c2a340408cec0bbe5ea523d30f
Robin Kim <therobinkim@gmail.com>
2018-03-23 15:51:28 -0500

    Add barebones React setup with minimal Webpack configs
    Signed-off-by: Robin Kim <therobinkim@gmail.com>

---
+++ code/part-two/client/source/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      Hello world!
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
