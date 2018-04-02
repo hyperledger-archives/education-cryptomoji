3ac2e1a3cd6fd4e9659f419a0f7f36d15041997b
Zac Delventhal <zac@bitwise.io>
2018-04-01 23:22:36 -0500

    Add tests for moji generation during Collection creation
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-two/processor/test/01-CreateCollection.js
+++ code/part-two/processor/test/01-CreateCollection.js
@@ -36,7 +36,75 @@ describe('Create Collection', function() {
         expect(collection.key, 'Collection should have a public key')
           .to.equal(publicKey);
         expect(collection.moji, 'Collection should have a moji array')
-          .to.deep.equal([]);
+          .to.be.an('array');
+      });
+  });
+
+  it('should create three moji for each new collection', function() {
+    return handler.apply(txn, context)
+      .then(() => {
+        const collection = decode(context.state[address]);
+        const mojiAddress = collection.moji[0];
+
+        expect(collection.moji, 'Collection should have three moji addresses')
+          .to.have.lengthOf(3);
+        expect(mojiAddress, 'Moji address should be 70 hex characters')
+          .to.match(/^[0-9a-f]{70}$/);
+        expect(context.state[mojiAddress], 'Moji should exist').to.exist;
+        const moji = decode(context.state[mojiAddress]);
+
+        expect(moji.dna, 'Moji DNA should be 36 hex characters')
+          .to.match(/^[0-9a-f]{36}$/);
+        expect(mojiAddress, 'Moji address match address generated from DNA')
+          .to.equal(getAddress.moji(publicKey)(moji.dna));
+      });
+  });
+
+  it('should create moji deterministically', function() {
+    let oldMoji = null;
+
+    return handler.apply(txn, context)
+      .then(() => {
+        const collection = decode(context.state[address]);
+        oldMoji = collection.moji;
+
+        // Delete the created collection and cryptomoji
+        oldMoji.concat(address).forEach(addr => delete context.state[addr] );
+
+        return handler.apply(txn, context);
+      })
+      .then(() => {
+        const collection = decode(context.state[address]);
+
+        expect(collection.moji, 'New moji should match old moji')
+          .to.deep.equal(oldMoji);
+      });
+  });
+
+  it('should create moji pseudorandomly', function() {
+    let oldMoji = null;
+
+    return handler.apply(txn, context)
+      .then(() => {
+        const collection = decode(context.state[address]);
+        oldMoji = collection.moji;
+
+        // Delete the created collection and cryptomoji
+        oldMoji.concat(address).forEach(addr => delete context.state[addr] );
+
+        // Modify a character in the signature to change the prng seed
+        const firstSig = txn.signature[0] !== 'f'
+          ? (parseInt(txn.signature[0], 16) + 1).toString(16)
+          : '0';
+        txn.signature = firstSig + txn.signature.slice(1);
+
+        return handler.apply(txn, context);
+      })
+      .then(() => {
+        const collection = decode(context.state[address]);
+
+        expect(collection.moji, 'Moji should not match when signature changes')
+          .to.not.deep.equal(oldMoji);
       });
   });
 
