b6bb1054a0f0039c4ce5761f9090cffb97698a2b
Zac Delventhal <zac@bitwise.io>
2018-04-28 14:40:56 -0500

    Add signing module tests for `createKeys`
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-two/client/tests/01-Signing.js
+++ code/part-two/client/tests/01-Signing.js
@@ -1,7 +1,45 @@
-describe('My Test', function() {
+import * as secp256k1 from 'secp256k1';
+import * as signing from '../source/services/signing.js';
 
-  it('should test', function() {
-    expect(1).to.equal(parseInt(1));
+
+describe('Signing module', function() {
+
+  describe('createKeys', function() {
+    let keys = null;
+
+    beforeEach(function() {
+      keys = signing.createKeys();
+    });
+
+    it('should return an object with two keys', function() {
+      expect(keys).to.have.property('privateKey');
+      expect(keys).to.have.property('publicKey');
+    });
+
+    it('should have keys that are hexadecimal strings', function() {
+      const { privateKey, publicKey } = keys;
+
+      expect(privateKey).to.be.a.hexString;
+      expect(publicKey).to.be.a.hexString;
+    });
+
+    it('should generate valid Secp256k1 keys', function() {
+      const privateKeyBytes = Buffer.from(keys.privateKey, 'hex');
+      const publicKeyBytes = Buffer.from(keys.publicKey, 'hex');
+
+      expect(secp256k1.privateKeyVerify(privateKeyBytes)).to.be.true;
+      expect(secp256k1.publicKeyVerify(publicKeyBytes)).to.be.true;
+    });
+
+    it('should have a public key derived from its private key', function() {
+      const privateKeyBytes = Buffer.from(keys.privateKey, 'hex');
+      const generatedPublicKey = secp256k1
+        .publicKeyCreate(privateKeyBytes)
+        .toString('hex');
+
+      expect(keys.publicKey).to.equal(generatedPublicKey);
+    });
+
   });
 
 });
--- code/part-two/client/tests/index.js
+++ code/part-two/client/tests/index.js
@@ -1,5 +1,13 @@
 import './01-Signing.js';
 
 
+chai.Assertion.addProperty('hexString', function() {
+  this.assert(
+    typeof this._obj === 'string' && /^[0-9a-f]*$/.test(this._obj),
+    'expected #{this} to be a hexadecimal string',
+    'expected #{this} to not be a hexadecimal string'
+  );
+});
+
 mocha.checkLeaks();
 mocha.run();
