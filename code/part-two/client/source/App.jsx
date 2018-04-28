8da60efa1f2bd0ee071ca475ef72d441424dae86
Robin Kim <therobinkim@gmail.com>
2018-04-28 09:28:19 -0500

    Remove <Browse /> and fakeApi module from <App />
    Signed-off-by: Robin Kim <therobinkim@gmail.com>

--- code/part-two/client/source/App.jsx
+++ code/part-two/client/source/App.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
-import { Browse } from './Browse';
 import { CreateOffer } from './CreateOffer';
-
-import { api } from './utils/fakeApi';
 
 export class App extends React.Component {
   render() {
@@ -13,18 +10,12 @@ export class App extends React.Component {
         <nav>
           <Link to="/">Home</Link>&ensp;
           <Link to="/createOffer">Create Offer</Link>&ensp;
-          <Link to="/browse/offers">View Offers</Link>&ensp;
-          <Link to="/browse">Browse</Link>
         </nav>
         <Switch>
           <Route
             exact
             path="/createOffer"
             component={CreateOffer}
-          />
-          <Route
-            path="/browse/:address?"
-            component={Browse}
           />
         </Switch>
       </div>
