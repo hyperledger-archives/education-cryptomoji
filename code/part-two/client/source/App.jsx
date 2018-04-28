80951490b33592e74c9b8b162884a51468dbe926
Robin Kim <therobinkim@gmail.com>
2018-04-28 09:32:04 -0500

    Refactor <App /> and <Moji /> to handle '/moji/:address' frontend routes
    Signed-off-by: Robin Kim <therobinkim@gmail.com>

--- code/part-two/client/source/App.jsx
+++ code/part-two/client/source/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 
 import { CreateOffer } from './CreateOffer';
+import { Moji } from './Moji';
 
 export class App extends React.Component {
   render() {
@@ -16,6 +17,11 @@ export class App extends React.Component {
             exact
             path="/createOffer"
             component={CreateOffer}
+          />
+          <Route
+            exact
+            path="/moji/:address"
+            component={Moji}
           />
         </Switch>
       </div>
--- code/part-two/client/source/Moji.jsx
+++ code/part-two/client/source/Moji.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { getMoji } from './services/requests';
+
 export class Moji extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      address: null,
+      moji: null
+    };
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log('GETDERIVEDSTATEFROMPROPS: <Moji />');
+    const address = nextProps.match.params.address;
+    return {
+      address
+    };
+  }
+
+  componentDidMount() {
+    console.log('COMPONENTDIDMOUNT: <Moji />');
+    this.fetchMoji(this.state.address);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('COMPONENTDIDUPDATE: <Moji />');
+    if (this.state.address !== prevState.address) {
+      this.fetchMoji(this.state.address);
+    }
+  }
+
+  fetchMoji(address) {
+    console.log('fetchMoji');
+    return getMoji(address)
+      .then(moji => this.setState({ moji }))
+      .catch(err => {
+        console.error(`Fetch moji failed for ${address}`, err);
+        this.setState({ moji: null });
+      });
+  }
+
   render() {
     console.log('RENDERING: <Moji />');
-    const { address, moji } = this.props;
+    const { address, moji } = this.state;
     if (!moji) {
       return (
         <div>
@@ -22,21 +62,21 @@ export class Moji extends React.Component {
             <tr><td>dna</td><td>{moji.dna}</td></tr>
             <tr>
               <td>owner</td>
-              <td><Link to={'/browse/' + moji.owner}>{moji.owner}</Link></td>
+              <td><Link to={'/collection/' + moji.owner}>{moji.owner}</Link></td>
             </tr>
             <tr>
               <td>sire</td>
-              <td><Link to={'/browse/' + moji.sire}>{moji.sire}</Link></td>
+              <td><Link to={'/moji/' + moji.sire}>{moji.sire}</Link></td>
             </tr>
             <tr>
               <td>breeder</td>
-              <td><Link to={'/browse/' + moji.breeder}>{moji.breeder}</Link></td>
+              <td><Link to={'/moji/' + moji.breeder}>{moji.breeder}</Link></td>
             </tr>
             <tr>
               <td>sired</td>
               <td>
                 {moji.sired.map(address => (
-                  <Link key={address} to={'/browse/' + address}>{address},</Link>
+                  <Link key={address} to={'/moji/' + address}>{address},</Link>
                 ))}
               </td>
             </tr>
@@ -44,7 +84,7 @@ export class Moji extends React.Component {
               <td>bred</td>
               <td>
                 {moji.bred.map(address => (
-                  <Link key={address} to={'/browse/' + address}>{address},</Link>
+                  <Link key={address} to={'/moji/' + address}>{address},</Link>
                 ))}
               </td>
             </tr>
