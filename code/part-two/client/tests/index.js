3dbeafe1805fd2de8cf15aa3295ea8d722f03a70
Zac Delventhal <zac@bitwise.io>
2018-04-29 12:04:15 -0500

    Add encoding module tests for `encode`
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-two/client/tests/index.js
+++ code/part-two/client/tests/index.js
@@ -1,4 +1,5 @@
 import './01-Signing.js';
+import './02-Encoding.js';
 
 
 chai.Assertion.addProperty('hexString', function() {
@@ -6,6 +7,14 @@ chai.Assertion.addProperty('hexString', function() {
     typeof this._obj === 'string' && /^[0-9a-f]*$/.test(this._obj),
     'expected #{this} to be a hexadecimal string',
     'expected #{this} to not be a hexadecimal string'
+  );
+});
+
+chai.Assertion.addProperty('bytes', function() {
+  this.assert(
+    this._obj instanceof Buffer || this._obj instanceof Uint8Array,
+    'expected #{this} to be a Buffer or Uint8array',
+    'expected #{this} to not be a Buffer or Uint8array'
   );
 });
 
