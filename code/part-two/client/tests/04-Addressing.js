15b8b41666a0d707124bfbe7559c246ed5ac1c56
Zac Delventhal <zac@bitwise.io>
2018-04-29 23:01:52 -0500

    Add addressing module tests for `getSireAddress`
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-two/client/tests/04-Addressing.js
+++ code/part-two/client/tests/04-Addressing.js
@@ -66,4 +66,29 @@ describe('Addressing module', function() {
 
   });
 
+  describe('getSireAddress', function() {
+    let publicKey = null;
+
+    beforeEach(function() {
+      publicKey = createKeys().publicKey;
+    });
+
+    it('should return a hexadecimal string', function() {
+      const address = addressing.getSireAddress(publicKey);
+      expect(address).to.be.a.hexString;
+    });
+
+    it('should return a correct Sire listing address', function() {
+      const address = addressing.getSireAddress(publicKey);
+      const keyHash = hash(publicKey).slice(0, 62);
+      expect(address).to.equal('5f4d76' + '02' + keyHash);
+    });
+
+    it('should return the type prefix if not passed a key', function() {
+      const address = addressing.getSireAddress();
+      expect(address).to.equal('5f4d76' + '02');
+    });
+
+  });
+
 });
