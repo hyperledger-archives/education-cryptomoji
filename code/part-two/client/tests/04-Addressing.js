8b576c82d5d63b48658ef946439575c49a645514
Zac Delventhal <zac@bitwise.io>
2018-04-29 22:59:11 -0500

    Add addressing module tests for `getMojiAddress`
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-two/client/tests/04-Addressing.js
+++ code/part-two/client/tests/04-Addressing.js
@@ -1,6 +1,6 @@
 import * as addressing from '../source/services/addressing.js';
 import { createKeys } from '../source/services/signing.js';
-import { createHash } from 'crypto';
+import { createHash, randomBytes } from 'crypto';
 
 
 const hash = str => createHash('sha512').update(str).digest('hex');
@@ -28,6 +28,40 @@ describe('Addressing module', function() {
     it('should return the type prefix if not passed a key', function() {
       const address = addressing.getCollectionAddress();
       expect(address).to.equal('5f4d76' + '00');
+    });
+
+  });
+
+  describe('getMojiAddress', function() {
+    let publicKey = null;
+    let dna = null;
+
+    beforeEach(function() {
+      publicKey = createKeys().publicKey;
+      dna = randomBytes(18).toString('hex');
+    });
+
+    it('should return a hexadecimal string', function() {
+      const address = addressing.getMojiAddress(publicKey, dna);
+      expect(address).to.be.a.hexString;
+    });
+
+    it('should return a correct Cryptomoji address', function() {
+      const address = addressing.getMojiAddress(publicKey, dna);
+      const keyHash = hash(publicKey).slice(0, 8);
+      const dnaHash = hash(dna).slice(0, 54);
+      expect(address).to.equal('5f4d76' + '01' + keyHash + dnaHash);
+    });
+
+    it('should return the Collection prefix if not passed dna', function() {
+      const address = addressing.getMojiAddress(publicKey);
+      const keyHash = hash(publicKey).slice(0, 8);
+      expect(address).to.equal('5f4d76' + '01' + keyHash);
+    });
+
+    it('should return the type prefix with no parameters', function() {
+      const address = addressing.getMojiAddress();
+      expect(address).to.equal('5f4d76' + '01');
     });
 
   });
