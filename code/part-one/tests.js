3186abd469c53eb8b9aecdd02cf4b589be1e19fc
Taylor Page <jonathantaylorpage@gmail.com>
2018-05-03 11:21:44 -0700

    Update PR based on requested changes
    - Remove irrelevant package-lock
    - Rename test previous hash variable
    - Remove 'has property' tests
    - Remove slice from hashing function
    - Move testing dependencies to dev env
    - Remove crypto dependencies
    
    Signed-off-by: Taylor Page <jonathantaylorpage@gmail.com>

--- code/part-one/tests.js
+++ code/part-one/tests.js
@@ -22,13 +22,13 @@ const miningAddress = hash('miningAddress');
 let timesMined = 0;
 
 // Test block data
-const testHash = '0123TESTHASH';
+const testPreviousHash = '0123testPreviousHash';
 const testTransactions = [];
 const testNonce = 0;
 const now = Date.now();
-const block = new Block(now, testTransactions, testHash);
+const block = new Block(now, testTransactions, testPreviousHash);
 const testDataHash = hash(
-  testHash + now + JSON.stringify(testTransactions) + testNonce
+  testPreviousHash + now + JSON.stringify(testTransactions) + testNonce
 );
 
 
@@ -45,15 +45,12 @@ describe('Transaction', () => {
 // Blockchain tests
 describe('Blockchain', () => {
   it('Should create a blockchain with a genesis block', done => {
-    expect(blockchain).to.have.property('_createGenesisBlock');
-    expect(blockchain).to.have.property('chain').with.lengthOf(1);
     expect(genesisBlock.previousHash).to.be.null;
     expect(genesisBlock.timestamp).is.not.null;
     done();
   });
 
   it('Should be able to retrieve the latest block', done => {
-    expect(blockchain).to.have.property('getLatestBlock');
     expect(blockchain.getLatestBlock()).to.equal(
       blockchain.chain[blockchain.chain.length - 1]
     );
@@ -63,8 +60,6 @@ describe('Blockchain', () => {
   it('Should be able to add pending transactions', done => {
     blockchain.addPendingTransaction(transaction);
 
-    expect(blockchain).to.have.property('addPendingTransaction');
-    expect(blockchain).to.have.property('_difficulty');
     expect(blockchain.pendingTransactions).to.have.lengthOf(1);
     done();
   });
@@ -73,7 +68,6 @@ describe('Blockchain', () => {
     blockchain.minePendingTransactions(miningAddress);
     timesMined++;
 
-    expect(blockchain).to.have.property('minePendingTransactions');
     expect(blockchain.chain).to.have.lengthOf(2);
     expect(blockchain.pendingTransactions).to.have.lengthOf(0);
     done();
@@ -82,13 +76,11 @@ describe('Blockchain', () => {
   it('Should be able to get balance for an address', done => {
     const addressBalance = blockchain.getBalanceOfAddress(miningAddress);
 
-    expect(blockchain).to.have.property('getBalanceOfAddress');
     expect(addressBalance).to.equal(blockchain.miningReward * timesMined);
     done();
   });
 
   it('Should create a valid blockchain', done => {
-    expect(blockchain).to.have.property('isValidChain');
     expect(blockchain.isValidChain()).to.be.true;
     done();
   });
@@ -98,18 +90,16 @@ describe('Blockchain', () => {
 describe('Block', () => {
   it('Should create a block with all initial values', done => {
     expect(block.timestamp).to.equal(now);
-    expect(block.previousHash).to.equal(testHash);
+    expect(block.previousHash).to.equal(testPreviousHash);
     done();
   });
 
   it('Should create a block that calculates hashes', done => {
-    expect(block).to.have.property('calculateHash');
     expect(block.hash).to.equal(testDataHash);
     done();
   });
 
   it('Should create a block that can be mined', done => {
-    expect(block).to.have.property('mineBlock');
 
     // If the initial hash meets the proof of work requirement
     if (block.hash[0] === '0') {
--- code/part-one/utils/helpers.js
+++ code/part-one/utils/helpers.js
@@ -4,8 +4,8 @@ const { createHash } = require('crypto');
 
 
 // Returns the first characters of a SHA-512 hash of a string
-const hash = (str, length = 128) => {
-  return createHash('sha512').update(str).digest('hex').slice(0, length);
+const hash = (str) => {
+  return createHash('sha512').update(str).digest('hex');
 };
 
 module.exports = {
