3c1e0e801e10ec69f99e82e062284edf0a3603b3
Taylor Page <jonathantaylorpage@gmail.com>
2018-05-03 13:17:20 -0700

    Create sign up form
    - Create form in component
    - Add event handlers
    
    Signed-off-by: Taylor Page <jonathantaylorpage@gmail.com>

--- code/part-two/client/source/Signup.jsx
+++ code/part-two/client/source/Signup.jsx
@@ -3,10 +3,50 @@ import React from 'react';
 export class Signup extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      email: '',
+      password: ''
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handlePasswordChange(event) {
+    this.setState({password: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
   }
 
   render() {
-    return (<div>Sign Up</div>);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Email
+          <input
+            name="email"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+        </label>
+        <input type="submit" value="sign up" />
+      </form>
+    );
   }
 }
