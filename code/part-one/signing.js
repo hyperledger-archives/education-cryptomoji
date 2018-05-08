f2324c1d7376e2a3bba111b67cdcec6c2c1c8454
Zac Delventhal <zac@bitwise.io>
2018-05-08 16:29:51 -0500

    Implement key creation methods for part-one signing module
    Adds the `createPrivateKey` and `getPublicKey` methods.
    
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-one/signing.js
+++ code/part-one/signing.js
@@ -1,10 +1,26 @@
 'use strict';
 
 const secp256k1 = require('secp256k1');
+const { randomBytes } = require('crypto');
 
 
-const createKeys = () => {
+// Returns a Buffer SHA-256 hash of a string or Buffer
+const sha256 = msg => createHash('sha256').update(msg).digest();
 
+// Converts a hex string to a Buffer
+const toBytes = hex => Buffer.from(hex, 'hex');
+
+const createPrivateKey = () => {
+  let privateKey = null;
+  do {
+    privateKey = randomBytes(32);
+  } while (!secp256k1.privateKeyVerify(privateKey));
+
+  return privateKey.toString('hex');
+};
+
+const getPublicKey = privateKey => {
+  return secp256k1.publicKeyCreate(toBytes(privateKey)).toString('hex');
 };
 
 const sign = (message, privateKey) => {
@@ -16,7 +32,8 @@ const verify = (message, signature, publicKey) => {
 };
 
 module.exports = {
-  createKeys,
+  createPrivateKey,
+  getPublicKey,
   sign,
   verify
 };
