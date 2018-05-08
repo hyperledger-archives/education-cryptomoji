2cfd74ca2b1dea50e34f1c3c0aa15fa9fa7d7649
Zac Delventhal <zac@bitwise.io>
2018-05-08 16:19:00 -0500

    Add `sign` tests for part-one signing modules
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-one/tests/01-Signing.js
+++ code/part-one/tests/01-Signing.js
@@ -2,8 +2,12 @@
 
 const { expect } = require('chai');
 const secp256k1 = require('secp256k1');
+const { randomBytes, createHash } = require('crypto');
 const signing = require('../signing.js');
 
+
+// Returns a Buffer SHA-256 hash of a string or Buffer
+const sha256 = msg => createHash('sha256').update(msg).digest();
 
 // Converts a hex string to a Buffer
 const toBytes = hex => Buffer.from(hex, 'hex');
@@ -28,7 +32,29 @@ describe('Signing module', function() {
   });
 
   describe('sign', function() {
+    const message = randomBytes(16);
+    let publicKey = null;
+    let signature = null;
 
+    beforeEach(function() {
+      const privateKey = signing.createPrivateKey();
+      publicKey = signing.getPublicKey(privateKey);
+      signature = signing.sign(privateKey, message);
+    });
+
+    it('should return a hex string', function() {
+      expect(signature).to.be.a.hexString;
+    });
+
+    it('should create a valid signature', function() {
+      const isValid = secp256k1.verify(
+        sha256(message),
+        toBytes(signature),
+        toBytes(publicKey)
+      );
+
+      expect(isValid).to.be.true;
+    });
   });
 
   describe('verify', function() {
