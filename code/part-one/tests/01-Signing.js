cc794395e42b710a1a731cac3da038ce63446758
Zac Delventhal <zac@bitwise.io>
2018-05-08 16:13:40 -0500

    Add stub signing module with stub test file to part one
    The concept of cryptographic signing is important to understand
    when approaching blockchains, and should be added to the
    blocks and transactions in part-one.
    
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

---
+++ code/part-one/tests/01-Signing.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { expect } = require('chai');
+const secp256k1 = require('secp256k1');
+const signing = require('../signing.js');
+
+
+describe('Signing module', function() {
+
+  describe('createKeys', function() {
+
+  });
+
+  describe('sign', function() {
+
+  });
+
+  describe('verify', function() {
+
+  });
+});
