d48d45a637e6e57b0e96916ac592ce9977335d39
Zac Delventhal <zac@bitwise.io>
2018-05-08 16:18:06 -0500

    Add part-one signing module tests for `createPrivateKey` method
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-one/tests/01-Signing.js
+++ code/part-one/tests/01-Signing.js
@@ -5,10 +5,26 @@ const secp256k1 = require('secp256k1');
 const signing = require('../signing.js');
 
 
+// Converts a hex string to a Buffer
+const toBytes = hex => Buffer.from(hex, 'hex');
+
 describe('Signing module', function() {
 
-  describe('createKeys', function() {
+  describe('createPrivateKey', function() {
+    let privateKey = null;
 
+    beforeEach(function() {
+      privateKey = signing.createPrivateKey();
+    });
+
+    it('should return a hex string', function() {
+      expect(privateKey).to.be.a.hexString;
+    });
+
+    it('should generate a valid Secp256k1 private key', function() {
+      const isValid = secp256k1.privateKeyVerify(toBytes(privateKey));
+      expect(isValid).to.be.true;
+    });
   });
 
   describe('sign', function() {
