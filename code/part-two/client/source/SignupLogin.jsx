77cfeae8d878bfc77dd4b66b3a2aeef90630f09b
Robin Kim <therobinkim@gmail.com>
2018-05-08 18:24:59 -0500

    Automatically generate collection upon (new) login
    Upon logging in, a `CREATE_COLLECTION` action will be fired.
    If the user has logged in before, this will operation will fail
    (which is ok). If this is the first time the user is logging in,
    they will now have a collection to view and interact with.
    
    The user will also be redirected to their collection upon login.
    
    Signed-off-by: Robin Kim <therobinkim@gmail.com>

--- code/part-two/client/source/SignupLogin.jsx
+++ code/part-two/client/source/SignupLogin.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { createKeys, getPublicKey } from './services/signing';
+import { submitPayloads } from './services/requests';
 
 export class SignupLogin extends React.Component {
   constructor(props) {
@@ -15,7 +16,7 @@ export class SignupLogin extends React.Component {
   }
 
   // ensures that when <App />'s private key changes (eg, set to null),
-  // those changes are received & rendered properly in this component
+  // those changes are received & rendered properly in the rendered <input />
   static getDerivedStateFromProps(nextProps, prevState) {
     return {
       privateKey: nextProps.privateKey
@@ -27,13 +28,22 @@ export class SignupLogin extends React.Component {
   }
 
   login(event) {
-    event.preventDefault();
+    if (event && event.preventDefault) {
+      event.preventDefault();
+    }
     const { privateKey } = this.state;
 
     try {
-      this.getPublicKeyFromPrivateKey(privateKey);
+      const publicKey = getPublicKey(privateKey);
+      this.props.setPublicKey(publicKey);
       this.props.setPrivateKey(privateKey);
-      this.props.history.push('/');
+      // rather than checking to see if collection exists and
+      // THEN creating it, always attempt to create a collection
+      // (to reduce the number of API requests)
+      this.createCollection(privateKey)
+        .finally(() => {
+          this.props.history.push('/collection/' + publicKey);
+        });
     } catch (err) {
       alert(
         'Log in failed!\n'
@@ -43,16 +53,15 @@ export class SignupLogin extends React.Component {
     }
   }
 
-  getPublicKeyFromPrivateKey(privateKey) {
-    const publicKey = getPublicKey(privateKey);
-    this.props.setPublicKey(publicKey);
+  generateNewPrivateKey() {
+    const { privateKey } = createKeys();
+    // replace contents of <input /> with newly generated private key,
+    // then login with that key
+    this.setState({ privateKey }, this.login);
   }
 
-  generateNewPrivateKey() {
-    const { privateKey, publicKey } = createKeys();
-    this.props.setPublicKey(publicKey);
-    this.props.setPrivateKey(privateKey);
-    this.props.history.push('/');
+  createCollection(privateKey) {
+    return submitPayloads(privateKey, { action: 'CREATE_COLLECTION' });
   }
 
   render() {
