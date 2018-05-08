99d68b66c46bb03057ca0bda0c237bab946122ae
Zac Delventhal <zac@bitwise.io>
2018-05-08 13:42:34 -0500

    Wrap blockchain tests in a parent describe block
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-one/tests/02-Blockchain.js
+++ code/part-one/tests/02-Blockchain.js
@@ -35,75 +35,78 @@ const testDataHash = hash(
   testPreviousHash + now + JSON.stringify(testTransactions) + testNonce
 );
 
-describe('Transaction', function() {
-  it('should create a transaction with all initial values', function() {
-    expect(transaction.fromAddress).to.equal(testFromAddress);
-    expect(transaction.toAddress).to.equal(testToAddress);
-    expect(transaction.amount).to.equal(testAmount);
-  });
-});
+describe('Blockchain module', function() {
 
-describe('Blockchain', function() {
-  it('should create a blockchain with a genesis block', function() {
-    expect(genesisBlock.previousHash).to.be.null;
-    expect(genesisBlock.timestamp).is.not.null;
+  describe('Transaction', function() {
+    it('should create a transaction with all initial values', function() {
+      expect(transaction.fromAddress).to.equal(testFromAddress);
+      expect(transaction.toAddress).to.equal(testToAddress);
+      expect(transaction.amount).to.equal(testAmount);
+    });
   });
 
-  it('should be able to retrieve the latest block', function() {
-    expect(blockchain.getLatestBlock()).to.equal(
-      blockchain.chain[blockchain.chain.length - 1]
-    );
+  describe('Blockchain', function() {
+    it('should create a blockchain with a genesis block', function() {
+      expect(genesisBlock.previousHash).to.be.null;
+      expect(genesisBlock.timestamp).is.not.null;
+    });
+
+    it('should be able to retrieve the latest block', function() {
+      expect(blockchain.getLatestBlock()).to.equal(
+        blockchain.chain[blockchain.chain.length - 1]
+      );
+    });
+
+    it('should be able to add pending transactions', function() {
+      blockchain.addPendingTransaction(transaction);
+
+      expect(blockchain.pendingTransactions).to.have.lengthOf(1);
+    });
+
+    it('should be able to mine pending transactions', function() {
+      blockchain.minePendingTransactions(miningAddress);
+      timesMined++;
+
+      expect(blockchain.chain).to.have.lengthOf(2);
+      expect(blockchain.pendingTransactions).to.have.lengthOf(0);
+    });
+
+    it('should be able to get balance for an address', function() {
+      const addressBalance = blockchain.getBalanceOfAddress(miningAddress);
+
+      expect(addressBalance).to.equal(blockchain.miningReward * timesMined);
+    });
+
+    it('should create a valid blockchain', function() {
+      expect(blockchain.isValidChain()).to.be.true;
+    });
   });
 
-  it('should be able to add pending transactions', function() {
-    blockchain.addPendingTransaction(transaction);
+  describe('Block', function() {
+    it('should create a block with all initial values', function() {
+      expect(block.timestamp).to.equal(now);
+      expect(block.previousHash).to.equal(testPreviousHash);
+    });
 
-    expect(blockchain.pendingTransactions).to.have.lengthOf(1);
-  });
-
-  it('should be able to mine pending transactions', function() {
-    blockchain.minePendingTransactions(miningAddress);
-    timesMined++;
-
-    expect(blockchain.chain).to.have.lengthOf(2);
-    expect(blockchain.pendingTransactions).to.have.lengthOf(0);
-  });
-
-  it('should be able to get balance for an address', function() {
-    const addressBalance = blockchain.getBalanceOfAddress(miningAddress);
-
-    expect(addressBalance).to.equal(blockchain.miningReward * timesMined);
-  });
-
-  it('should create a valid blockchain', function() {
-    expect(blockchain.isValidChain()).to.be.true;
-  });
-});
-
-describe('Block', function() {
-  it('should create a block with all initial values', function() {
-    expect(block.timestamp).to.equal(now);
-    expect(block.previousHash).to.equal(testPreviousHash);
-  });
-
-  it('should create a block that calculates hashes', function() {
-    expect(block.hash).to.equal(testDataHash);
-  });
-
-  it('should create a block that can be mined', function() {
-
-    // If the initial hash meets the proof of work requirement
-    if (block.hash[0] === '0') {
+    it('should create a block that calculates hashes', function() {
       expect(block.hash).to.equal(testDataHash);
-      expect(block._nonce).to.equal(testNonce);
+    });
 
-    // If not uses nonce to generate proof of work
-    } else {
-      block.mineBlock(1);
+    it('should create a block that can be mined', function() {
 
-      expect(block.hash).to.not.equal(testDataHash);
-      expect(block._nonce).to.not.equal(testNonce);
-    }
+      // If the initial hash meets the proof of work requirement
+      if (block.hash[0] === '0') {
+        expect(block.hash).to.equal(testDataHash);
+        expect(block._nonce).to.equal(testNonce);
 
+      // If not uses nonce to generate proof of work
+      } else {
+        block.mineBlock(1);
+
+        expect(block.hash).to.not.equal(testDataHash);
+        expect(block._nonce).to.not.equal(testNonce);
+      }
+
+    });
   });
 });
