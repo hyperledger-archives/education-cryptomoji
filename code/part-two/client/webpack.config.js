d22d3f3781dce3f6c8a9c64c8f21a17e68d53660
Robin Kim <therobinkim@gmail.com>
2018-05-07 23:51:16 -0500

    Fix webpack config for babel-loader to not run for json files
    Signed-off-by: Robin Kim <therobinkim@gmail.com>

--- code/part-two/client/webpack.config.js
+++ code/part-two/client/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?/,
+        test: /\.jsx?$/,
         include: SOURCE_DIR,
         use: {
           loader: 'babel-loader',
