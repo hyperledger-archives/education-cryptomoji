e32b551911562fa84e37957e718bb3bda91f01f3
Zac Delventhal <zac@bitwise.io>
2018-05-12 13:53:17 -0500

    Add `isValidTransaction` tests to part-one
    Adds a new stub module to part-one: validation. This will include
    methods for validation each blockchain structure, as well as one method
    to break the blockchain (for fun).
    
    These initial tests cover this `isValidTransaction` method.
    
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

---
+++ code/part-one/validation.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { createHash } = require('crypto');
+const signing = require('./signing');
+
+const isValidTransaction = transaction => {
+
+};
+
+const isValidBlock = block => {
+
+};
+
+const isValidChain = blockchain => {
+
+};
+
+const breakChain = blockchain => {
+
+};
+
+module.exports = {
+  isValidTransaction,
+  isValidBlock,
+  isValidChain,
+  breakChain
+};
