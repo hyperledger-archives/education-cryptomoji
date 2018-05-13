8e3e89ea2675c522e8461e2d8528e490c27edad4
Zac Delventhal <zac@bitwise.io>
2018-05-12 22:12:36 -0500

    Rename services/parseDna to services/parse_dna
    Previously, parseDna was the only lowerCamelCased js fiename. Now
    it matches the other filenames which are snake_case.
    
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-two/client/source/MojiItem.jsx
+++ code/part-two/client/source/MojiItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { getMoji } from './services/requests';
-import { parseDna } from './services/parseDna';
+import { parseDna } from './services/parse_dna';
 
 export class MojiItem extends React.Component {
   constructor(props) {
