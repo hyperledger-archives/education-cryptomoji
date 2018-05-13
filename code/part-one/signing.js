fe360f1385bee64df61756a46b9c5457759f54a3
Zac Delventhal <zac@bitwise.io>
2018-05-12 20:03:16 -0500

    Finalize doc strings for part-one
    Only modifies comments, no functionality changes.
    
    Signed-off-by: Zac Delventhal <zac@bitwise.io>

--- code/part-one/signing.js
+++ code/part-one/signing.js
@@ -10,6 +10,15 @@ const sha256 = msg => createHash('sha256').update(msg).digest();
 // Converts a hex string to a Buffer
 const toBytes = hex => Buffer.from(hex, 'hex');
 
+/**
+ * A function which generates a new random Secp256k1 private key, returning
+ * it as a 64 character hexadecimal string.
+ *
+ * Example:
+ *   const privateKey = createPrivateKey();
+ *   console.log(privateKey);
+ *   // 'e291df3eede7f0c520fddbe5e9e53434ff7ef3c0894ed9d9cbcb6596f1cfe87e'
+ */
 const createPrivateKey = () => {
   let privateKey = null;
   do {
@@ -19,15 +28,51 @@ const createPrivateKey = () => {
   return privateKey.toString('hex');
 };
 
+/**
+ * A function which takes a hexadecimal private key and returns its public pair
+ * as a 66 character hexadecimal string.
+ *
+ * Example:
+ *   const publicKey = getPublicKey(privateKey);
+ *   console.log(publicKey);
+ *   // '0202694593ddc71061e622222ed400f5373cfa7ea607ce106cca3f039b0f9a0123'
+ *
+ * Hint:
+ *   Remember that the secp256k1 library expects raw bytes (i.e Node.js
+ *   Buffers), not hex strings! You'll have to convert the private key.
+ */
 const getPublicKey = privateKey => {
   return secp256k1.publicKeyCreate(toBytes(privateKey)).toString('hex');
 };
 
+/**
+ * A function which takes a hex private key and a string message, returning
+ * a 128 character hexadecimal signature.
+ *
+ * Example:
+ *   const signature = sign(privateKey, 'Hello World!');
+ *   console.log(signature);
+ *   // '4ae1f0b20382ad628804a5a66e09cc6bdf2c83fa64f8017e98d84cc75a1a71b52...'
+ *
+ * Hint:
+ *   Remember that you need to sign a SHA-256 hash of the message,
+ *   not the message itself!
+ */
 const sign = (privateKey, message) => {
   const { signature } = secp256k1.sign(sha256(message), toBytes(privateKey));
   return signature.toString('hex');
 };
 
+/**
+ * A function which takes a hex public key, a string message, and a hex
+ * signature, and returns either true or false.
+ *
+ * Example:
+ *   console.log( verify(publicKey, 'Hello World!', signature) );
+ *   // true
+ *   console.log( verify(publicKey, 'Hello World?', signature) );
+ *   // false
+ */
 const verify = (publicKey, message, signature) => {
   return secp256k1.verify(
     sha256(message),
