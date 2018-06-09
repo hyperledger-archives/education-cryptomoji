68bb34cdcab04786533ef82f8679dfbf10a43232
Robin Kim <therobinkim@gmail.com>
2018-06-09 13:14:07 -0500

    Add Bootstrap syling to <NavBar />
    Signed-off-by: Robin Kim <therobinkim@gmail.com>

--- code/part-two/client/source/NavBar.jsx
+++ code/part-two/client/source/NavBar.jsx
@@ -1,27 +1,72 @@
 /* SOLUTION FILE */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink  } from 'react-router-dom';
 
 export function NavBar({ publicKey, logout }) {
   return (
-    <nav>
-      <Link to="/">Home</Link>&ensp;
-      {
-        publicKey &&
-        (
-          <Link to={'/collection/' + publicKey}>
-            Your Collection
-          </Link>
-        )
-      }
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <span className="navbar-brand mb-0 h1">Cryptomoji</span>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-      <Link to="/collection">View Collections</Link>&ensp;
-      <Link to="/offer">View Offers</Link>&ensp;
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className="navbar-nav">
+          {
+            publicKey &&
+            (
+              <NavLink
+                exact
+                to={'/collection/' + publicKey}
+                className="nav-item nav-link"
+                activeClassName="active"
+              >Yours</NavLink>
+            )
+          }
 
-      <Link to="/signup-login">
-        {publicKey ? 'View Private Key' : 'Sign Up/Login'}
-      </Link>&ensp;
-      {publicKey && <a href="#" onClick={logout}>Logout</a>}
+          <NavLink
+            exact
+            to="/collection"
+            className="nav-item nav-link"
+            activeClassName="active"
+          >Collections</NavLink>
+
+          <NavLink
+            exact
+            to="/offer"
+            className="nav-item nav-link"
+            activeClassName="active"
+          >Offers</NavLink>
+
+          <NavLink
+            exact
+            to="/signup-login"
+            className="nav-item nav-link"
+            activeClassName="active"
+          >
+            {publicKey ? 'Private Key' : 'Sign Up/Login'}
+          </NavLink>
+
+          {
+            publicKey &&
+            (
+              <a
+                href="#"
+                onClick={logout}
+                className="nav-item nav-link"
+              >Logout</a>
+            )
+          }
+        </div>
+      </div>
     </nav>
   );
 }
